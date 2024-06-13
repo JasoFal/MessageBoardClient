3c7c40ad67d760b84ef44b5afd635a4ca9c89765
Jason Falk <jasonfalk9@gmail.com>
2024-06-13 10:04:59 -0700

    Add base scaffolded template.

---
+++ MessageClient/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace MessageClient.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
