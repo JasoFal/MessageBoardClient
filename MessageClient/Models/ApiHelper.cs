95f5c89cc15bfda81a6e8b1d2caec5c2605fd06f
Jason Falk <jasonfalk9@gmail.com>
2024-06-17 11:08:15 -0700

    Add get method.

--- MessageClient/Models/ApiHelper.cs
+++ MessageClient/Models/ApiHelper.cs
@@ -12,5 +12,13 @@ namespace MessageClient.Models
       RestResponse response = await client.GetAsync(request);
       return response.Content;
     }
+
+    public static async Task<string> Get(int id)
+    {
+      RestClient client = new RestClient("http://localhost:5003/");
+      RestRequest request = new RestRequest($"api/messages/{id}", Method.Get);
+      RestResponse response = await client.GetAsync(request);
+      return response.Content;
+    }
   }
 }
