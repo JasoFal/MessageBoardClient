847022cf18f579d6032c888620cfaf395837ef6f
Jason Falk <jasonfalk9@gmail.com>
2024-06-17 09:40:55 -0700

    Add get command to pull specific messages.

--- MessageClient/Models/Message.cs
+++ MessageClient/Models/Message.cs
@@ -2,10 +2,11 @@ using System.Collections.Generic;
 using System;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
+using System.ComponentModel.DataAnnotations;
 
 namespace MessageClient.Models
 {
-  public class MessageClient
+  public class Message
   {
     public int MessageId { get; set; }
     public string Content { get; set; }
@@ -15,15 +16,23 @@ namespace MessageClient.Models
 
     public static List<Message> GetMessages()
     {
-      var apiCallTask = ApiHelper.GetAll();
-      var result = apiCallTask.Result;
+      Task<string> apiCallTask = ApiHelper.GetAll();
+      string result = apiCallTask.Result;
 
-      JArray jsonResponse = JsonConvert.Deserialized<JArray>(result);
-      List<Message> messageList = JsonConvert.DeserializedObject<List<Message>>(jsonResponse.ToString());
+      JArray jsonResponse = JsonConvert.DeserializeObject<JArray>(result);
+      List<Message> messageList = JsonConvert.DeserializeObject<List<Message>>(jsonResponse.ToString());
 
       return messageList;
     }
 
-    
+    public static Message GetDetails(int id)
+    {
+      Task<string> apiCallTask = ApiHelper.Get(id);
+      string result = apiCallTask.Result;
+
+      JObject jsonResponse = JsonConvert.DeserializeObject<JObject>(result);
+      Message message = JsonConvert.DeserializeObject<Message>(jsonResponse.ToString());
+      return message;
+    }
   }
 }
