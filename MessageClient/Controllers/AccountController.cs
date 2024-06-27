9a55df208a9d650174d1794079952f3c294021f9
Jason Falk <jasonfalk9@gmail.com>
2024-06-27 14:50:34 -0700

    Add log in and log off routes.

--- MessageClient/Controllers/AccountController.cs
+++ MessageClient/Controllers/AccountController.cs
@@ -8,11 +8,13 @@ namespace MessageClient.Controllers
 {
   public class AccountController : Controller
   {
+    private readonly MessageClientContext _db;
     private readonly UserManager<ApplicationUser> _userManager;
     private readonly SignInManager<ApplicationUser> _signInManager;
 
-    public AccountController (UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager)
+    public AccountController (UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, MessageClientContext db)
     {
+      _db = db;
       _userManager = userManager;
       _signInManager = signInManager;
     }
@@ -51,6 +53,40 @@ namespace MessageClient.Controllers
           return View(model);
         }
       }
+    }
+
+    public ActionResult Login()
+    {
+      return View();
+    }
+
+    [HttpPost]
+    public async Task<ActionResult> Login(LoginViewModel model)
+    {
+      if (!ModelState.IsValid)
+      {
+        return View(model);
+      }
+      else
+      {
+        Microsoft.AspNetCore.Identity.SignInResult result = await _signInManager.PasswordSignInAsync(model.Email, model.Password, isPersistent: true, lockoutOnFailure: false);
+        if (result.Succeeded)
+        {
+          return RedirectToAction("Index");
+        }
+        else
+        {
+          ModelState.AddModelError("",  "There is something wrong with your email or password. Please try again.");
+          return View(model);
+        }
+      }
+    }
+
+    [HttpPost]
+    public async Task<ActionResult> LogOff()
+    {
+      await _signInManager.SignOutAsync();
+      return RedirectToAction("Index");
     }
   }
 }
